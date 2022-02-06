d5ee8075d608a56867630fbaddb967752972644d
Gustavo Sampaio <sampaioglima@gmail.com>
2022-02-06 15:57:22 -0300

    scopes and the global keyword

--- module6/code.py
+++ module6/code.py
@@ -8,6 +8,7 @@ def compose_banner(message, outer_border="-"):
 banner = compose_banner("Python is sassy")
 print(banner)
 
+
 # A consequence of python's only-once-at-runtime evaluation policy
 def add_ingredients(recipe, ingredients=[]):
     ingredients.append("eggs")
@@ -16,4 +17,20 @@ def add_ingredients(recipe, ingredients=[]):
     print(recipe)
     print(ingredients)
 
-add_ingredients("ingredients")
+# add_ingredients("ingredients")
+
+
+# The global keyword in python scoping
+weigth_to_lift_in_kg = 20
+def change_weigth(new_weight):
+    global weigth_to_lift_in_kg
+    weigth_to_lift_in_kg = new_weight
+    print("weight changed!")
+
+
+def show_current_weigth_charge():
+    print(weigth_to_lift_in_kg, "kg are being lifted at the moment.")
+
+show_current_weigth_charge()
+change_weigth(25)
+show_current_weigth_charge()
