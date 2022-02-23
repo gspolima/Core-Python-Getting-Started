5f4e2096660672ec3626015505a28d14af8eef02
Gustavo Sampaio <sampaioglima@gmail.com>
2022-02-23 14:43:41 -0300

    collections exercises

--- module7/code.py
+++ module7/code.py
@@ -1,7 +1,31 @@
-def splitStringBySeparator(string: str, separator: str):
+from typing import List, Tuple
+
+
+def split_string_by_separator(string: str, separator: str):
     splitted = string.split(separator)
     joined = ' '.join(splitted)
     return joined
 
-result = splitStringBySeparator('Emma-is-a-data-scientist','-')
-print(result)
+# print(split_string_by_separator('Emma-is-a-data-scientist','-'))
+
+
+def turn_tuple_into_string(tuple: Tuple):
+    result = ' '.join(tuple)
+    return result
+
+# print(turn_tuple_into_string(('This', 'is', 'a', 'tuple')))
+
+
+def replace_4th_and_4th_to_last_element(list: List):
+    if (list.__len__() < 7):
+        return None
+
+    if (list[-4] != 42):
+        list[-4] = 42
+
+    if (list[3] != 68):
+        list[3] = 68
+
+    return list
+
+print(replace_4th_and_4th_to_last_element([1, 3, 12, 40, 94, 97, 91, 89]))
